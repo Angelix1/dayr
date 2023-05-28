c40047825beda9033a02332125bc1bd2acb93436
Angel <86276165+Angelix1@users.noreply.github.com>
2023-05-28 10:45:04 +0700

    Create ad_obj_list.lua

---
+++ src/1.764/lib/ad_obj_list.lua
@@ -0,0 +1,106 @@
+return {
+	[1] = {
+		['need'] = {
+			[1] = {
+				[1] = [[ad_tv]],
+				[2] = 1,
+				[3] = true
+			}
+		},
+		['cooldownId'] = [[ad_tv]],
+		['id'] = [[ad_tv]],
+		['isAdTv'] = true
+	},
+	[2] = {
+		['isShop'] = true,
+		['cooldownId'] = [[shop]],
+		['id'] = [[shop]],
+		['isAdTv'] = true
+	},
+	[3] = {
+		['isUnlock'] = true,
+		['cooldownId'] = [[lock]],
+		['id'] = [[lock]]
+	},
+	[4] = {
+		['isRFFree'] = true,
+		['cooldownId'] = [[season]],
+		['itemId'] = [[emba_ad_box]],
+		['need'] = {
+			[1] = {
+				[1] = [[emba_ad_box]],
+				[2] = 1,
+				[3] = true
+			}
+		},
+		['give'] = {
+			[1] = {
+				[1] = [[flashlight]],
+				[2] = 1
+			},
+			[2] = {
+				[1] = [[cocktail_molotov]],
+				[2] = 5
+			},
+			[3] = {
+				[1] = [[injector_green]],
+				[2] = 1
+			}
+		},
+		['id'] = [[ad_emba1]]
+	},
+	[5] = {
+		['isRFFree'] = true,
+		['cooldownId'] = [[season]],
+		['itemId'] = [[emba_ad_box]],
+		['need'] = {
+			[1] = {
+				[1] = [[emba_ad_box]],
+				[2] = 1,
+				[3] = true
+			}
+		},
+		['give'] = {
+			[1] = {
+				[1] = [[emba_herbicide]],
+				[2] = 1
+			},
+			[2] = {
+				[1] = [[emba_fuel]],
+				[2] = 3
+			},
+			[3] = {
+				[1] = [[injector_blue]],
+				[2] = 1
+			}
+		},
+		['id'] = [[ad_emba2]]
+	},
+	[6] = {
+		['isRFFree'] = true,
+		['cooldownId'] = [[season]],
+		['itemId'] = [[emba_ad_box]],
+		['need'] = {
+			[1] = {
+				[1] = [[emba_ad_box]],
+				[2] = 1,
+				[3] = true
+			}
+		},
+		['give'] = {
+			[1] = {
+				[1] = [[emba_herbicide]],
+				[2] = 3
+			},
+			[2] = {
+				[1] = [[emba_fuel]],
+				[2] = 3
+			},
+			[3] = {
+				[1] = [[injector_purple]],
+				[2] = 1
+			}
+		},
+		['id'] = [[ad_emba3]]
+	}
+}
